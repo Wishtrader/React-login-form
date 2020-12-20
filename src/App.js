f0e7f7ba756e70132dcd04baa19b46c607ce6bd4
Andrei Kamarou <mr.akomarov@gmail.com>
2020-12-20 04:23:27 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,5 @@ import "./styles.css";
 import styled from "styled-components";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <div className="App"></div>;
 }
