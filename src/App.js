268ff4cfe8a9ca5f63811ddd81298ee93fc2bcfc
Andrei Kamarou <mr.akomarov@gmail.com>
2020-12-20 04:19:51 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import styled from "styled-components";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
